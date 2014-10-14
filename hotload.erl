c4d9e5b8ddb00eed23daf934193a0e7a2b748788
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-10-14 13:14:13 +0400

    initial

---
+++ hotload.erl
@@ -0,0 +1,15 @@
+-module(hotload).
+-export([server/1, upgrade/1]).
+
+server(State) ->
+    receive
+        update ->
+            NewState = ?MODULE:upgrade(State),
+            ?MODULE:server(NewState);  %% loop in the new version of the module
+        SomeMessage ->
+            %% do something here
+            server(State)  %% stay in the same version no matter what.
+    end.
+
+upgrade(OldState) ->
+    ok.  %% transform and return the state here.
